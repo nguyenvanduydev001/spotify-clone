8d305e99bf3541509f896415df4c99d8bf8c0f58
Little bird <156684740+nguyenvanduydev001@users.noreply.github.com>
2024-07-26 14:32:25 +0700

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from "./pages/ListAlbum";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
-export const url = 'http://localhost:4000'
+export const url = 'https://spotify-clone-admin-w7of.onrender.com'
 
 const App = () => {
   return (
